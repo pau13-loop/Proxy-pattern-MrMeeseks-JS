a7a6fc3ec35a15a16c21efbde17b22a7220fe488
pau13-loop <pllinas@cifpfbmoll.eu>
2021-11-09 14:52:29 +0100

    feat: :sparkles: Complete second user story

--- src/box/box.js
+++ src/box/box.js
@@ -1,7 +1,12 @@
+const meeseks = require('../mrMeeseks/mrMeeseks')
 
 function Box() {
     this.name = "Rick's box";
     this.mrMeeseks = null;
+}
+
+Box.prototype.createMrMeeseeks = function () {
+    return JSON.parse( JSON.stringify( meeseks ) );
 }
 
 var factory = (function singleBox() {
